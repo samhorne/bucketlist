b6bf942716c45d0c21fba504453e1d6de6578f06
Sam Horne <samuel.horne@life.church>
2022-04-13 20:26:11 -0500

    implement picasso to cache images

--- app/src/main/java/com/example/carddemo/Book.kt
+++ app/src/main/java/com/example/carddemo/Book.kt
@@ -4,7 +4,6 @@ var bookList = mutableListOf<Book>()
 
 class Book(
     var cover: Int,
-    var author: String,
     var title: String,
     var description: String,
     var id: Int? = bookList.size
--- app/src/main/java/com/example/carddemo/CardViewHolder.kt
+++ app/src/main/java/com/example/carddemo/CardViewHolder.kt
@@ -2,13 +2,15 @@ package com.example.carddemo
 
 import androidx.recyclerview.widget.RecyclerView
 import com.example.carddemo.databinding.CardCellBinding
+import com.squareup.picasso.Picasso
 
 class CardViewHolder(
     private val cardCellBinding: CardCellBinding
 ): RecyclerView.ViewHolder(cardCellBinding.root) {
     fun bindBook(book: Book){
         cardCellBinding.cover.setImageResource(book.cover)
-        cardCellBinding.title.text = book.title
-        cardCellBinding.author.text = book.author
+        val picasso = Picasso.get()
+        picasso.load("https://www.commonwealthfund.org/sites/default/files/styles/countries_hero_desktop/public/country_image_Japan.jpg")
+            .into(cardCellBinding.cover)
     }
 }
